d917510e2500142f0960b056b18ac3e904253687
Viktor <gogolviktor5@gmail.com>
2025-05-05 22:39:23 +0200

    Lesson  4.1. JavaScript primitive data types, Home Task

---
+++ jsBasics/task2.js
@@ -0,0 +1,5 @@
+let name1 ="Vlad"
+let name2 = "Anna"
+let congrats = `Hello ${name1} and ${name2}`;
+
+console.log(congrats)
---
+++ jsBasics/task3.js
@@ -0,0 +1,4 @@
+let age = 15;
+let checkAge = age >= 16;
+
+console.log(checkAge);
